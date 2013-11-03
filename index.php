50a194a62c295309f3087c45f692a2c4bf536543
Anderson Malta <anderson.malta@sky.com>
2013-11-03 20:13:43 +0000

    adding changes

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>P2 Hello World!</h1>
+<html>
+<head>
+	<style>
+    hr {color:sienna;}
+    h1 {font-family:Verdana, Geneva, sans-serif; font-size:2em; border-radius:3em; border-color:#CC3333; border:thin}
+    p {margin-left:20px;}
+    body {background-color:#FFCCCC}
+    </style>
+</head>
+<body>
+	<h1>P2 Hello World!</h1>
+	<p style="color:sienna;margin-left:20px;">Hello Hello</p>
+</body>
+</html>
